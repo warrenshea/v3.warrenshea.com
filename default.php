a676fa5417a043efd53a7806c68f1c7f782fd457
warrenshea <warren.shea@gmail.com>
2016-06-28 13:41:50 -0400

    Fixed <? to <?php due to errors

--- default.php
+++ default.php
@@ -1,22 +1,25 @@
-<?
-$srvr_http_user_agent = $_SERVER['HTTP_USER_AGENT'];
-$old = false;
-if ((stristr($srvr_http_user_agent,"MSIE 6.0") == true) || (stristr($srvr_http_user_agent,"MSIE 7.0") == true) || (stristr($srvr_http_user_agent,"MSIE 8.0") == true)) {
-  $old = true;
-}
-if ($_GET["feed"] == "rss2") {
-  header("Expires:Mon, 31 Dec 2010 05:00:00 GMT");
-  header("Content-type: text/xml");
-  $page_contents = file_get_contents("http://www.worldofwarren.com/?feed=rss2");
-  echo $page_contents;
-} else if ($_GET["feed"] == "comments-rss2") {
-  header("Expires:Mon, 31 Dec 2010 05:00:00 GMT");
-  header("Content-type: text/xml");
-  $page_contents = file_get_contents("http://www.worldofwarren.com/?feed=comments-rss2");
-  echo $page_contents;
-} ?>
+<?php
+  $srvr_http_user_agent = $_SERVER['HTTP_USER_AGENT'];
+  $old = false;
+  if ((stristr($srvr_http_user_agent,"MSIE 6.0") == true) || (stristr($srvr_http_user_agent,"MSIE 7.0") == true) || (stristr($srvr_http_user_agent,"MSIE 8.0") == true)) {
+    $old = true;
+  }
+  if ($_GET["feed"]) {
+    if ($_GET["feed"] == "rss2") {
+      header("Expires:Mon, 31 Dec 2010 05:00:00 GMT");
+      header("Content-type: text/xml");
+      $page_contents = file_get_contents("http://www.worldofwarren.com/?feed=rss2");
+      echo $page_contents;
+    } else if ($_GET["feed"] == "comments-rss2") {
+      header("Expires:Mon, 31 Dec 2010 05:00:00 GMT");
+      header("Content-type: text/xml");
+      $page_contents = file_get_contents("http://www.worldofwarren.com/?feed=comments-rss2");
+      echo $page_contents;
+    }
+  }
+?>
 <!doctype html>
-<? if ($old) {?><html><? } else { ?><html lang="en-ca"><? } echo "\n";?>
+<?php if ($old) {?><html><?php } else { ?><html lang="en-ca"><?php } echo "\n";?>
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=9;IE=8;IE=7;IE=6;IE=EDGE;chrome=1">
@@ -26,11 +29,11 @@ if ($_GET["feed"] == "rss2") {
     <meta name="author" content="Warren Shea">
     <title>Warren Shea</title>
     <link rel="Icon" type="image/png" href="common/images/favicon.png">
-    <? if ($old) {?><link rel="stylesheet" href="common/proxy.php?url=stylesheet.css&type=css"><? } else { ?><link rel="stylesheet" href="common/stylesheet.css"><? } echo "\n"; ?>
+    <?php if ($old) {?><link rel="stylesheet" href="common/proxy.php?url=stylesheet.css&type=css"><?php } else { ?><link rel="stylesheet" href="common/stylesheet.css"><?php } echo "\n"; ?>
   </head>
   <body>
-<? if ($old) {?>    <script>alert ('This site uses HTML5 which is not supported with the browser you are using. Some items may not function as intended. Please use a modern browser (Chrome, Firefox, Safari, Edge or Internet Explorer 11) for the full experience.');</script><? echo "\n"; ?><? } ?>
-<? if ($old) {?>
+<?php if ($old) {?>    <script>alert ('This site uses HTML5 which is not supported with the browser you are using. Some items may not function as intended. Please use a modern browser (Chrome, Firefox, Safari, Edge or Internet Explorer 11) for the full experience.');</script><?php echo "\n"; ?><?php } ?>
+<?php if ($old) {?>
     <div id="header">
       <div id="nav">
         <ul id="nav-text"></ul>
@@ -38,7 +41,7 @@ if ($_GET["feed"] == "rss2") {
       </div>
       <div id="header-content"></div>
       <div id="title"></div>
-    </div><? } else { ?>
+    </div><?php } else { ?>
     <header>
       <nav>
         <ul id="nav-text"></ul>
@@ -46,7 +49,7 @@ if ($_GET["feed"] == "rss2") {
       </nav>
       <div id="header-content"></div>
       <div id="title"></div>
-    </header><? } echo "\n"; ?>
+    </header><?php } echo "\n"; ?>
     <div id="showcontent-container"></div>
     <div id="container-background"></div>
     <div id="container">
@@ -56,7 +59,7 @@ if ($_GET["feed"] == "rss2") {
       </div>
       <div id="content-footer"></div>
     </div>
-    <? if ($old) {?><div id="footer"><div id="footer-content"></div></div><? } else { ?><footer><div id="footer-content"></div></footer><? } echo "\n"; ?>
-    <? if ($old) {?><script src="common/proxy.php?url=scripts.js&type=js"></script><? } else { ?><script src="common/scripts.js"></script><? } echo "\n"; ?>
+    <?php if ($old) {?><div id="footer"><div id="footer-content"></div></div><?php } else { ?><footer><div id="footer-content"></div></footer><?php } echo "\n"; ?>
+    <?php if ($old) {?><script src="common/proxy.php?url=scripts.js&type=js"></script><?php } else { ?><script src="common/scripts.js"></script><?php } echo "\n"; ?>
   </body>
 </html>
